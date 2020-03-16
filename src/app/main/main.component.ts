c1069cc835ff5619592cbbb1724f8f3449b2ba89
Bryce <bykerbry@umich.edu>
2020-03-16 00:38:16 -0400

    Upgraded to Angular 9 & added error handeling

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { PostService } from '../post.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms'
+import { HttpClient } from '@angular/common/http';
 import { MatAutocompleteTrigger } from '@angular/material';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
+import { map, catchError} from "rxjs/operators";
+import { PostService } from '../post.service';
 import { IPost } from '../post.interface';
+
 
 @Component({
   selector: 'app-main',
@@ -16,14 +19,14 @@ export class MainComponent implements OnInit {
 
   postArr$: Observable<IPost[]>
   subredditTitle: string;
-
+  error: boolean;
   suggestions: any;
   suggestionsArr: string[];
   subredditControl = new FormControl('');
 
   @ViewChild(MatAutocompleteTrigger, {static: true}) autocomplete: MatAutocompleteTrigger;
 
-  constructor(private _postService: PostService) { }
+  constructor(private http: HttpClient, private _postService: PostService) { }
 
   ngOnInit() {
     this.getSubreddit('aww');
@@ -32,9 +35,33 @@ export class MainComponent implements OnInit {
   getSubreddit(subreddit: string) {
     this.subredditTitle = subreddit.substring(0,1).toUpperCase() + subreddit.substring(1);
     subreddit = subreddit.replace(' ', '');
-    this.postArr$ = this._postService.getPosts(subreddit)
+    this.postArr$ = this.getPosts(subreddit)
     this.autocomplete.closePanel();
   };
+
+  getPosts(subreddit: string) {
+    return this.http.get('https://www.reddit.com/r/' + subreddit + '/.json?limit=10').pipe(
+      map((res: any) => {
+        let postArr: IPost[] = [];
+        res.data.children.forEach((post: any) => {
+           postArr.push({
+            title: post.data.title,
+            imgUrl: post.data.thumbnail,
+            imgH: post.data.thumbnail_height,
+            imgW: post.data.thumbnail_width,
+            link: post.data.url
+          });
+        });
+        this.error = false;
+        return postArr;
+      }),
+      catchError(err => {
+        console.log('There was an error.',err);
+        throwError(err)
+        this.error = true;
+        return of([])
+      })
+  )}
 
   getSuggestions(currentText: string) {
     this._postService.getAutoComplete(currentText).subscribe( response => {
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, catchError } from "rxjs/operators";
-import { IPost } from './post.interface'
-import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,29 +9,6 @@ import { throwError } from 'rxjs';
 export class PostService {
 
   constructor(private http: HttpClient) { }
-
-  getPosts(subreddit: string) {
-    return this.http.get('https://www.reddit.com/r/' + subreddit + '/.json?limit=10').pipe(
-      map((res: any) => {
-        let postArr: IPost[] = [];
-        res.data.children.forEach((post: any) => {
-           postArr.push({
-            title: post.data.title,
-            imgUrl: post.data.thumbnail,
-            imgH: post.data.thumbnail_height,
-            imgW: post.data.thumbnail_width,
-            link: post.data.url
-          });
-        });
-        return postArr;
-      }),
-      catchError(err => this.errorHandler(err))
-  )}
-
-  errorHandler(error: any) {
-    console.log(error);
-    return throwError(error)
-  }
   
   getAutoComplete(userText: string) {
     return this.http.get("http://www.reddit.com/api/subreddit_autocomplete/.json?include_over_18=true&include_profiles=false&query=" + userText)
